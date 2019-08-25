7dbf2710863bbff1c3a29d3446a5db153aea1db3
Arunesh Saxena <asaxena24@sapient.com>
2019-08-25 12:31:11 +0530

    firebase dynamic data integrated

--- js/db.js
+++ js/db.js
@@ -1,7 +1,14 @@
 // real-time listener
 db.collection('recipes').onSnapshot(snapshot => {
-    console.log(snapshot.docChanges());
-    console.log(change.type, change.doc.id, change.doc.data());
-    
+    //console.log(snapshot.docChanges());
+    snapshot.docChanges().forEach(change => {
+      //console.log(change.type, change.doc.id, change.doc.data());
+      if(change.type === 'added'){
+        renderRecipe(change.doc.data(), change.doc.id);
+      }
+      if(change.type === 'removed'){
+        // remove the document data from the web page
+      }
+    });
   });
   
