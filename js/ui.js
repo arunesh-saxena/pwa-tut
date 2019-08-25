516329e89be7f0f830f26bd39873b6c845a986ff
Arunesh Saxena <asaxena24@sapient.com>
2019-08-25 13:24:48 +0530

    delete item offline by firebase

--- js/ui.js
+++ js/ui.js
@@ -1,12 +1,12 @@
 const recipes = document.querySelector('.recipes');
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
-  M.Sidenav.init(menus, {edge: 'right'});
+  M.Sidenav.init(menus, { edge: 'right' });
   // add recipe form
   const forms = document.querySelectorAll('.side-form');
-  M.Sidenav.init(forms, {edge: 'left'});
+  M.Sidenav.init(forms, { edge: 'left' });
 });
 
 // render recipe data
@@ -26,4 +26,10 @@ const renderRecipe = (data, id) => {
   `;
   recipes.innerHTML += html;
 
+};
+
+// remove recipe
+const removeRecipe = (id) => {
+  const recipe = document.querySelector(`.recipe[data-id=${id}]`);
+  recipe.remove();
 };
